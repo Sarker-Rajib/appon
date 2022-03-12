051a6637cc25e0e46329306671c37a0f4c85814e
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-03-13 00:09:03 +0600

    a

---
+++ assets/js/custom.js
@@ -0,0 +1,12 @@
+$('.autoplay').slick({
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   autoplay: true,
+   speed: 3000,
+   fade: true,
+   dots: true,
+   cssEase: 'linear',
+   arrows: false,
+   draggable: true,
+   pauseOnHover: false,   
+ });
